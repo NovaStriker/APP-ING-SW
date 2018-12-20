b2458cbfff76baf0406c349fa188f8d70fc477e0
NovaStriker <johnaldoc@hotmail.com>
2018-12-20 16:21:47 -0500

    Initial commit

---
+++ app/src/main/java/ec/comnet/httpwww/comnetapp/Pantalla_pagos_cliente.java
@@ -0,0 +1,13 @@
+package ec.comnet.httpwww.comnetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla_pagos_cliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_pagos_cliente);
+    }
+}
---
+++ app/src/main/java/ec/comnet/httpwww/comnetapp/Pantalla_principal_cliente.java
@@ -0,0 +1,13 @@
+package ec.comnet.httpwww.comnetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla_principal_cliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_principal_cliente);
+    }
+}
